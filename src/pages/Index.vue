2e03517105f43372ae27fd72eeebe9c029571fb1
BatelT <bately77@gmail.com>
2021-12-30 13:59:49 +0200

    text

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,23 +3,108 @@
     
     <h2>What is Build&nbsp;Info</h2>
     <p>
-    
-Simply think about the idea that you can get fully traceable information for your build that describes  all the details about the build. Broken down into segments the build is a version of a program; and the info is all of the details contained within the build - artifacts, project modules, dependencies - all of the items that create the build itself. When you put the two together you get a detailed view of your code and the components that created it. It is a snapshot into the inner workings of your code. 
-When we have all the information collected by the build agent we will have the list of project modules, artifacts, dependencies, environment variables and more.
-</p>
+    Build-info is the metadata of a build. It includes all the details about the build broken down into segments that include version history, artifacts, project modules, dependencies, and everything that was required to create the build. In short, it is a snapshot of the components used to build your application, collected by the build agent.
+    </p>
     <g-image
       class="img-fluid"
       src="~/assets/images/homepage/content/first-image.png"
     />
 
-    <h2>Why Do I Care</h2>
+    <h2>Why Do I Care About Metadata</h2>
     <p>
-    With the build info we have the power to analyze our builds we can easily get the information about artifacts that we are using, the time it took to run the build and much more. With this information we can improve quality to make our build faster and even help us with some Cyber attacks. Because 
-Open Source Software is amazing but it is not without its faults or vulnerabilities. Free code can make your build run smoothly but it can also contain active threats to your project, code, business, customers, and overall software environment. This has been proven time and time again with the recent hacks to SolarWinds, the Colonial Pipeline Hack, Equifax and many others. This is why you should care about build info. Knowing what metadata is contained within your build can help you identify known vulnerabilities and malicious code. This is one of the many reasons why we are so happy to be able to share Build Info with you - so you can have one more layer of security to your code. 
-    </p>
-    
-    <h2>How Do I Get Build Info?</h2>
-    <p>realized the need for developers and companies to review and evaluate their build on the meta level. One of the core features of JFrog’s flagship tool, Artifactory, is build-info. JFrog is committed to being a community first company and these recent attacks and the increased sophistication of malicious hackers was a catalyst for Yoav to decide to break out Build-Info from Artifactory and offer it as an open source solution to reviewing the metadata in your build. </p>
+With the build-info we can easily achieve traceability of our builds, giving us the power to analyze them and get the information about the artifacts we’re using. Mainly where they’re coming from and how they are being used. This information is crucial to helping us improve the quality of our builds as well as ensure their security.
+    </p>    
+    <h2>What information is stored in the build info</h2>
+    <p>Build-info includes all the metadata for our binary lifecycle. It is all the information collected by the build agent which includes details about the build. The build-info includes the list of project modules, artifacts, dependencies, environment variables and more. When using this open source tool, the client collects the build-info and outputs the information in a JSON format. </p>
+<vue-code-highlight language="json">
+  <pre>
+{
+  "agent": {},
+  "buildAgent": {
+    "name": "GENERIC"
+  },
+  "modules": [
+    {
+      "type": "go",
+      "id": "github.com/jfrog/build-info-go",
+      "dependencies": [
+        {
+          "id": "github.com/pmezard/go-difflib:v1.0.0",
+          "type": "zip",
+          "sha1": "f200e2a5211b527ef2d2ff301718ccc4ad5c705b",
+          "md5": "fb72df530a7f3fca56ccc192c9f30a58"
+        },
+        {
+          "id": "github.com/russross/blackfriday/v2:v2.0.1",
+          "type": "zip",
+          "sha1": "afd8cfd78a268f5aaa7b86924145c333ea65c603",
+          "md5": "8b04dcc4504ca8943c91a4b6cc59cda3"
+        },
+        {
+          "id": "github.com/stretchr/testify:v1.7.0",
+          "type": "zip",
+          "sha1": "53b5c82ff76628b33b04017e8c81fbc1875f5737",
+          "md5": "3cb74476ca750cb267db738a4db2f534"
+        },
+        {
+          "id": "github.com/urfave/cli/v2:v2.3.0",
+          "type": "zip",
+          "sha1": "0f882edb17acb1c544f6d53c5afa1d6d2add1308",
+          "md5": "81a81c77ec9b2721e0229a66d5f77a83"
+        },
+        {
+          "id": "gopkg.in/check.v1:v0.0.0-20161208181325-20d25e280405",
+          "type": "zip",
+          "sha1": "66ade2d7ec84edaaa9b95e00f79f00b5ac5b1ca2",
+          "md5": "5937b6bf7a94c4e0e10c98c7ae7cb7ea"
+        },
+        {
+          "id": "gopkg.in/yaml.v3:v3.0.0-20200313102051-9f266ea9e77c",
+          "type": "zip",
+          "sha1": "ec896ba2dc97dc3aa33066686b74259520428e00",
+          "md5": "b8faa9934f8e54c43766ce7b4b2e0d49"
+        },
+        {
+          "id": "github.com/cpuguy83/go-md2man/v2:v2.0.0-20190314233015-f79a8a8ca69d",
+          "type": "zip",
+          "sha1": "5586c962d5149ce9d73190ae61bab99ed56d4c7f",
+          "md5": "ca2d6e511be9be839f06e049e710063e"
+        },
+        {
+          "id": "github.com/shurcoo!l/sanitized_anchor_name:v1.0.0",
+          "type": "zip",
+          "sha1": "fd4810a945b887a2e0f0ebb760131e13dca566ae",
+          "md5": "90b29aa5c53c3df1b2b80e4d7220b1e3"
+        },
+        {
+          "id": "github.com/davecgh/go-spew:v1.1.0",
+          "type": "zip",
+          "sha1": "d3e0e482506ae14c36fdf2dfb57f796235d87319",
+          "md5": "6b9ae88321948b256d8110455142e498"
+        },
+        {
+          "id": "github.com/jfrog/gofrog:v1.1.1",
+          "type": "zip",
+          "sha1": "438ad3217d4ccbcb20bca8bfa5c1aa5aa704f9ed",
+          "md5": "dc8cea2a1424c6abd4af2a74d2e680e2"
+        }
+      ]
+    }
+  ],
+  "started": "2021-12-20T14:25:52.747+0200"
+}
+  </pre>
+</vue-code-highlight>
+    <h2>Comparing your builds</h2>
+<p>In case your build fails, the build-info can help debug any issues by comparing to a previous build. Doing this comparison will allow you to investigate the changes such as new artifacts added, dependencies deleted and more.
+</p>
+ <h2>Security risks with open source software</h2>
+<p>Open Source Software is amazing but it is not without its faults or vulnerabilities. Free code can make your build run smoothly but it can also contain active threats to your project, code, business, customers, and overall software environment. This has been proven time and time again with the recent hacks to SolarWinds, the Colonial Pipeline Hack, Equifax and many others. Knowing what metadata is contained within your build can help you identify known vulnerabilities and malicious code.</p>
+ <h2>How Do I Get the BuildInfo?</h2>
+<p>The build-info capability is a core functionality that is part of the JFrog Platform. The JFrog team has now made it available to the community as a separate component enabling you to easily retrieve the build-info for your builds.
+
+To get started, download the build-info open source project. </p>
+
     <g-image
       class="img-fluid"
       src="~/assets/images/homepage/content/sec-image.gif"
@@ -28,13 +113,14 @@ Open Source Software is amazing but it is not without its faults or vulnerabilit
 </template>
 
 <script>
+import { component as VueCodeHighlight } from 'vue-code-highlight';
 export default {
   metaInfo: {
     title: 'JFrog Build Info'
+  },
+  components: {
+    VueCodeHighlight
   }
 }
 </script> 
-
-<style>
-
-</style>
+<style src="vue-code-highlight/themes/duotone-sea.css"></style>
